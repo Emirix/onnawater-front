0226ba0a338d15c1e551e7ad8ac5c119a6dfa1e6
Emir Tanır <emirtanir55@gmail.com>
2025-03-31 04:57:21 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
             <div  className='cursor-pointer'>
                 <div className="flex gap-2 mt-3 justify-center">
                     <img src='images/logo/fav.svg' className="w-[60px]" alt="" />
-                    <div className="text-white font-bold text-5xl">ONNA ROBOTICS</div>
+                    <div className="text-white font-bold text-5xl">ONNA ROBOTICSs</div>
                 </div>
                 <div className="ps-[32px] text-center  text-white/70 font-bold text-1xl tracking-[9px] 	ms-5">UNDERWATER TECHNOLOGY</div>
             </div>
